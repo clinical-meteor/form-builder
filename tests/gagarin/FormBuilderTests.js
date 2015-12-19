6942a85c336b1694273f172078653ec875093d38
Abigail Watson <awatson1978@gmail.com>
2015-12-19 14:23:21 -0800

    FormBuilderTests.js

--- tests/gagarin/FormBuilderTests.js
+++ tests/gagarin/FormBuilderTests.js
@@ -27,4 +27,26 @@ describe('clinical:form-builder', function () {
       expect(FormBuilder).to.exist;
     });
   });
+
+
+  it("FormBuilder can add partial schemas to a Form.", function () {
+    return client.execute(function () {
+
+      expect(Items.find().count()).to.equal(0);
+
+      Session.set('selectedFormElementId', "textInputBlock");
+
+      FormBuilder.addQuestion("Why did the chicken cross the road?");
+
+      var item = Items.findOne();
+
+      expect(item.formElementId).to.equal("textInputBlock");
+      expect(item.elementType).to.equal("text");
+      expect(item.rank).to.equal(0);
+      expect(item.schemaTemplate.type).to.equal("String");
+      expect(item.schemaTemplate.autoform.afFieldInput.type).to.equal("text");
+    });
+  });
+
+
 });
